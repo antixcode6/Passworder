ec1713ed5a7ad3d4157f9c2f390c41fdbbfa3927
antixcode6 <ed.aponte0@gmail.com>
2016-04-20 22:01:11 -0400

    Update Main.py
    Getting towards goal

--- Main.py
+++ Main.py
@@ -1,26 +1,55 @@
 import base64
 import os
 import sys
-import curses
+import random
+import subprocess
 
 
-def __Size_Check__(number):
+def __size_check__(number): #Some would say this is useless - but I say to hell with bad test cases
     try:
         if(int(number) <= 2):
-            print "Number must be greater than two (2)..."
+            print "Too little words selected: must select more than two (2)"
             return False
+        elif(int(number) >= 8):
+            print "Too many words selected: must select less than eight (8)"
     except ValueError:
-        print "Not a valid type: must enter whole number"
         return False
     else:
         return True
 
-print "Passworder Program"
-pass_size = int(raw_input("Password Size: "))
+def __get_pass__():
+    words = list(open('text.txt'))
+    random.SystemRandom().shuffle(words)
+    target = open("pass.txt",'w')
+    target.write((' '.join(w.strip() for w in words[:4])))
 
-if(__Size_Check__(pass_size) == False):
 
-    __Size_Check__(pass_size)
+
+def main():
+    print "Passworder Program"
+    pass_size = int(raw_input("Password Size: "))
+
+    if(__size_check__(pass_size) == False):
+        sys.exit()
+    else:
+        __get_pass__()
+        '''
+         open("text.txt", 'a')
+        if sys.platform == "win32":
+            os.startfile("text.txt")
+        else:
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
+            subprocess.call([opener, "text.txt"])
+        '''
+
+
+
+
+    sys.exit()
+if __name__ == '__main__':
+  main()
+
+
 
 
 
