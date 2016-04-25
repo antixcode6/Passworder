40ba714bfd9cee1884d9cf96bea4086690b4ccdf
antixcode6 <ed.aponte0@gmail.com>
2016-04-24 23:19:58 -0400

    Updated main.py
    Removed some modules from file

--- Main.py
+++ Main.py
@@ -1,47 +1,31 @@
-import base64
 import os
 import sys
-import random
 import subprocess
-
+from scrape import __scrape_from_web__
 
 def __size_check__(number):
     try:
         if(int(number) <= 2):
             print "Too little words selected: must select more than two (2)"
             return False
-        elif(int(number) >= 8):
-            print "Too many words selected: must select less than eight (8)"
+        elif(int(number) >= 11):
+            print "Too many words selected: must select less than eleven (11)"
     except ValueError:
         return False
     else:
         return True
 
-def __get_pass__(p_size):
-    words = list(open('text.txt'))
-    random.SystemRandom().shuffle(words)
-    target = open("pass.txt",'w')
-    wodries = (' '.join(w.strip() for w in words[:p_size]))
-    target.write(wodries)
-
-
 
 def main():
-    print "Password size must be between 2 and 8"
+    print "Passworder Program"
     pass_size = int(raw_input("Password Size: "))
 
     if(__size_check__(pass_size) == False):
         sys.exit()
     else:
-        __get_pass__(pass_size)
-
-        open("text.txt", 'a')
-        if sys.platform == "win32":
-            os.startfile("pass.txt")
-        else:
-            opener = "open" if sys.platform == "darwin" else "xdg-open"
-            subprocess.call([opener, "pass.txt"])
+        __scrape_from_web__(pass_size)
     sys.exit()
+
 if __name__ == '__main__':
   main()
 
