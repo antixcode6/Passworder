332c25a5840247af2409946e55db41faea4ffd18
Eddie <ed.aponte0@gmail.com>
2016-12-25 10:28:36 -0800

    Update Main.py
    Refactored code for input error checking for first stage of application removed function calls for further refactoring to web scraping logic

--- Main.py
+++ Main.py
@@ -3,29 +3,31 @@ import sys
 import subprocess
 from scrape import __scrape_from_web__
 
-def __size_check__(number):
-    try:
-        if(int(number) <= 2):
-            print "Too little words selected: must select more than two (2)"
-            return False
-        elif(int(number) >= 11):
-            print "Too many words selected: must select less than eleven (11)"
-    except ValueError:
-        return False
-    else:
-        return True
+def repeatOnError(*exceptions):
+  def checking(function):
+    def checked(*args, **kwargs):
+      while True:
+        try:
+          result = function(*args, **kwargs)
+        except exceptions as problem:
+          print "There was a problem with the input:"
+          print problem.__class__.__name__
+          print problem
+          print "Please enter a valid base-10 number\nWe support password generation for 3 to 25 words!"
+        else:
+          return result
+    return checked
+  return checking
 
 
 def main():
-    print "Passworder Program"
-    pass_size = int(raw_input("Password Size: "))
+   @repeatOnError(ValueError)
+    def getNumber():
+      return int(raw_input("Please enter your password length: "))
 
-    if(__size_check__(pass_size) == False):
-        sys.exit()
-    else:
-        __scrape_from_web__(pass_size)
-    sys.exit()
-
+    passCounter = getNumber()
+    print "You have chosen a ", passCounter, " word password."
+    
 if __name__ == '__main__':
   main()
 
